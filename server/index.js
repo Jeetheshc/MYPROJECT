e63c0b3ccae25a8d5ee37e1099ee34a815ebae06
JEETHESH <jeetheshc@gmail.com>
2024-12-22 23:39:28 +0530

    mm1

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(
     cors({
         
-        origin: ["http://localhost:5173"] ,
+        origin: ["https://carrentapp.vercel.app"] ,
         credentials: true,
         methods: ["GET","POST","PATCH","PUT","DELETE"]
     })
